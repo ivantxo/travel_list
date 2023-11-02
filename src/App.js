afbe877e1efa7ff53e0844a3f9cf6017440277c1
IvanR <ivan.ramirezf@gmail.com>
2023-11-03 05:39:07 +1000

    Added form and handlesubmit skeleton

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😀 trip</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -32,7 +36,7 @@ function Form() {
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
