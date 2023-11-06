84375c8624afdefe279e7d59db4f8b459fbc40d1
IvanR <ivan.ramirezf@gmail.com>
2023-11-07 05:47:30 +1000

    Early return when no items in stats section

--- src/App.js
+++ src/App.js
@@ -107,6 +107,14 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
   const numItems = items.length;
   const numPacked = items.filter((i) => i.packed).length;
   const percentage = Math.round((numPacked * 100) / numItems);
@@ -116,9 +124,7 @@ function Stats({ items }) {
       <em>
         {percentage === 100
           ? "Everything is ready to go ✈️"
-          : `🧳 You have ${numItems} items on your list, and you have packed ${numPacked} (${
-              percentage ? percentage : 0
-            }%)`}
+          : `🧳 You have ${numItems} items on your list, and you have packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
